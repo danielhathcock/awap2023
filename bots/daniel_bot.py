ab3c83caccbc5312f36020fb0b72f7056e40598e
Daniel Hathcock <danielchathcock98@gmail.com>
2023-02-18 03:49:21 -0500

    Some fixes for daniel bot

--- bots/daniel_bot.py
+++ bots/daniel_bot.py
@@ -12,11 +12,24 @@ class BotPlayer(Player):
     def __init__(self, team: Team):
         self.team = team
 
+        self.game_state = None
+        self.game_info = None
+        self.map_width = None
+        self.map_height = None
+
         self.et_pairs : list[tuple[RobotInfo, RobotInfo]] = []
-        self.construct_state = 0
+        self.creating_pair = 0
+
+        self.assigned_mines = set()
+
+        self.new_exp = None
 
         return
 
+
+
+    def no_collision(self, row, col):
+        return self.game_state.get_map()[row][col].robot is None
 
     def isvalid(self, row, col):
         if row < 0 : return False
@@ -41,32 +54,34 @@ class BotPlayer(Player):
         val : int = 0
         d_options : list = []
         for d in Direction:
-            if self.game_state.can_move_robot(rname, d):
+            if self.game_state.can_move_robot(rname, d) and self.no_collision(robot_row+d.value[0], robot_col+d.value[1]):
                 cur : int = self.get_explorable_tiles(robot_row + d.value[0], robot_col + d.value[1])
-                if cur > val and self.game_state.get_map()[robot_row+d.value[0]][robot_col+d.value[1]].robot is None:
+                if cur > val:
                     val = cur
                     d_options = []
                     d_options.append(d)
                     continue
-                if cur == val and self.game_state.get_map()[robot_row+d.value[0]][robot_col+d.value[1]].robot is None:
+                if cur == val:
                     d_options.append(d)
                     continue
-        d_move = random.choice(d_options)
-        self.game_state.move_robot(rname, d_move)
-        if self.game_state.can_robot_action(rname):
-            self.game_state.robot_action(rname)
+        if len(d_options) > 0:
+            d_move = random.choice(d_options)
+            self.game_state.move_robot(rname, d_move)
+            if self.game_state.can_robot_action(rname):
+                self.game_state.robot_action(rname)
+            return d_move
 
 
     def explore_action(self, game_state: GameState) -> None:
         '''Perform one move/action sequence for each of the explore/terraform pairs'''
-        print(self.et_pairs)
+        # print(self.et_pairs)
         for exp, ter in self.et_pairs:
             if exp.battery == 0:
                 # Recharge sequence
                 for d in Direction:
                     dest_row = ter.row + d.value[0]
                     dest_col = ter.col + d.value[1]
-                    if game_state.can_move_robot(ter.name, d) and game_state.get_map()[dest_row][dest_col].robot is None:
+                    if game_state.can_move_robot(ter.name, d) and self.no_collision(dest_row,dest_col):
                         game_state.move_robot(ter.name, d)
                         if game_state.can_robot_action(ter.name):
                             game_state.robot_action(ter.name)
@@ -76,12 +91,27 @@ class BotPlayer(Player):
             else:
                 # Explore sequence
                 old_exp_row, old_exp_col = (exp.row, exp.col)
-                self.explore_next(exp.name, exp)
+                d_move = self.explore_next(exp.name, exp)
 
                 # Move Terraformer to the previous location of the explorer
-                game_state.move_robot(ter.name, Direction((old_exp_row - ter.row, old_exp_col - ter.col)))
+                if d_move is not None:
+                    game_state.move_robot(ter.name, Direction((old_exp_row - ter.row, old_exp_col - ter.col)))
                 if game_state.can_robot_action(ter.name):
                     game_state.robot_action(ter.name)   
+
+
+    def nearest_terr_tile(self, row, col):
+        '''get nearest terraformed tile (dummy)'''
+        return (0, 0)
+
+    def create_et_pair(self, row, col):
+        '''Create a pair nearest to row, col'''
+        if self.creating_pair == 0:
+            self.creating_pair = 1
+
+        if self.creating_pair == 1 and self.game_state.get_metal() >= 50:
+            nearest = self.nearest_terr_tile(row, col)
+            
 
 
 
@@ -125,27 +155,31 @@ class BotPlayer(Player):
             exp, ter = self.et_pairs[i]
             self.et_pairs[i] = (robots[exp.name], robots[ter.name])
 
-        if self.construct_state == 0:
+        self.explore_action(game_state)
+
+        
+        if self.creating_pair != 0:
+            self.create_et_pair()
+        
+        
+        if self.construct_state == 0 and self.game_info.metal >= 100:
             for spawn_loc in ally_tiles:
                 if self.construct_state > 0:
                     break
                 spawn_type = RobotType.EXPLORER
                 if game_state.can_spawn_robot(spawn_type, spawn_loc.row, spawn_loc.col):
-                    game_state.spawn_robot(spawn_type, spawn_loc.row, spawn_loc.col)
+                    self.new_exp = game_state.spawn_robot(spawn_type, spawn_loc.row, spawn_loc.col)
                     self.construct_state = 1
 
         elif self.construct_state == 1:
-            exp_name, exp = list(robots.items())[0]
+            exp_name, exp = self.new_exp.name, robots[self.new_exp.name]
             self.explore_next(exp_name, exp)
 
             if game_state.can_spawn_robot(RobotType.TERRAFORMER, exp.row, exp.col):
                 new_ter = game_state.spawn_robot(RobotType.TERRAFORMER, exp.row, exp.col)
-                self.construct_state = 2
+                self.construct_state = 0
                 self.et_pairs.append((exp, new_ter))
-
-            print(self.et_pairs)
-        else:
-            self.explore_action(game_state)
+        
 
         # iterate through dictionary of robots
         for rname, rob in game_state.get_ally_robots().items():
